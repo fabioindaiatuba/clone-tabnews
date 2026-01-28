e13610f089ee6107ceb7472bd5338de55e8971a6
Fabio Gonçalves <fabioindaiatuba@outlook.com>
2026-01-28 08:24:09 -0300

    feat: implement /api/v1/user endpoint with session renewal

--- src/infra/controller.js
+++ src/infra/controller.js
@@ -1,3 +1,5 @@
+import * as cookie from "cookie";
+import session from "models/session";
 import {
   InternalServerError,
   MethodNotAllowedError,
@@ -27,11 +29,25 @@ function onErrorHandler(error, request, response) {
   response.status(publicErrorObject.statusCode).json(publicErrorObject);
 }
 
+function setSessionCookie(sessionToken, response) {
+  const setCookie = cookie.serialize("session_id", sessionToken, {
+    path: "/", // Caminho nde o cookie ficara disponivel
+    // no navegador / seria en todo o nosso site
+    // expires: new Date(newSession.expires_at), // Data de expiracao mas nao usado pois usa o MaxAge
+    maxAge: session.EXPIRATION_IN_MILLISECONDS / 1000, // mas em caso de data errada no navegador
+    // usa o max-age para em segundos a partir da data do navegador
+    secure: process.env.NODE_ENV === "production", // Grava o cookie porem o retorno so é feito se for https quando true
+    httpOnly: true, // Fica acessivel somente no navegador em comando javascript nao funciona.
+  });
+  response.setHeader("Set-Cookie", setCookie);
+}
+
 const controller = {
   errorHandler: {
     onNoMatch: onNoMatchHandler,
     onError: onErrorHandler,
   },
+  setSessionCookie,
 };
 
 export default controller;
--- src/pages/api/v1/sessions/index.js
+++ src/pages/api/v1/sessions/index.js
@@ -1,4 +1,3 @@
-import * as cookie from "cookie";
 import controller from "infra/controller.js";
 import authentication from "models/authentication";
 import session from "models/session";
@@ -19,17 +18,6 @@ async function postHandler(request, response) {
   );
 
   const newSession = await session.create(authenticatedUser.id);
-
-  const setCookie = cookie.serialize("session_id", newSession.token, {
-    path: "/", // Caminho nde o cookie ficara disponivel
-    // no navegador / seria en todo o nosso site
-    // expires: new Date(newSession.expires_at), // Data de expiracao mas nao usado pois usa o MaxAge
-    maxAge: session.EXPIRATION_IN_MILLISECONDS / 1000, // mas em caso de data errada no navegador
-    // usa o max-age para em segundos a partir da data do navegador
-    secure: process.env.NODE_ENV === "production", // Grava o cookie porem o retorno so é feito se for https quando true
-    httpOnly: true, // Fica acessivel somente no navegador em comando javascript nao funciona.
-  });
-  response.setHeader("Set-Cookie", setCookie);
-
+  controller.setSessionCookie(newSession.token, response);
   return response.status(201).json(newSession);
 }
